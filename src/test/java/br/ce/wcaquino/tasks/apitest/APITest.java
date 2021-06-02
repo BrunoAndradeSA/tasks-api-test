9f4a3d4a081fd90affd69ed0f16dd6526896a111
Bruno de Andrade Simões de Almeida <bruno.a.s.almeida@gmail.com>
2021-06-02 00:26:03 -0300

    Data futura para os testes

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -8,7 +8,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class APITest {
-	
+
 	@BeforeClass
 	public static void setup() {
 		RestAssured.baseURI = "http://localhost:8088/tasks-backend";
@@ -16,34 +16,20 @@ public class APITest {
 
 	@Test
 	public void deveRetornarTarefas() {
-		RestAssured.given()
-		.when()
-			.get("/todo")
-		.then()
-			.statusCode(200);
+		RestAssured.given().when().get("/todo").then().statusCode(200);
 	}
-	
+
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
-		RestAssured.given()
-			.body("{\"task\": \"Teste via RestAssured\",\"dueDate\": \"2021-06-01\"}")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201);
+		RestAssured.given().body("{\"task\": \"Teste via RestAssured\",\"dueDate\": \"2022-06-01\"}")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(201);
 	}
-	
+
 	@Test
 	public void naoDeveAdicionarTarefaInvalida() {
-		RestAssured.given()
-			.body("{\"task\": \"Teste via RestAssured\",\"dueDate\": \"2010-05-29\"}")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(400)
-			.body("message", CoreMatchers.is("Due date must not be in past"));
+		RestAssured.given().body("{\"task\": \"Teste via RestAssured\",\"dueDate\": \"2010-05-29\"}")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(400)
+				.body("message", CoreMatchers.is("Due date must not be in past"));
 	}
-	
+
 }
